efb697ab432b4e2c3651e32d89bbe6309303eaa2
Albert <jxjfifa@gmail.com>
2023-05-23 00:21:33 -0300

    Reparing the identation in js file

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,55 +1,54 @@
 const alternatives = ["rock", "paper", "scissors"];
-            let playerChoice;
+let playerChoice;
 
-            function getComputerChoice(){
-                return Math.floor(Math.random() * 3);
-            }
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
             
-            function playRound(choice){
-                choice = prompt("Type your choice: \n"+
-                "Rock, Paper or Scissors").toUpperCase();
+function playRound(choice){
+    choice = prompt("Type your choice: \n"+
+    "Rock, Paper or Scissors").toUpperCase();
 
-                let computerSelection=getComputerChoice();
+    let computerSelection=getComputerChoice();
 
-                if(choice!=="ROCK"&&choice!=="PAPER"&&choice!=="SCISSORS"){
-                    return alert("Not a valid choice! Thats an end game :(");
-                }
-
-                switch(choice){
-                    case "ROCK":
-                        if(computerSelection==1){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[0]+"\n"
-                                            +"You have lost the game :(\n");
-                        }
-                        if(computerSelection==2){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[0]+"\n"
-                                            +"You have win the game :D\n");
-                        }
-                    case "PAPER":
-                        if(computerSelection==2){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[1]+"\n"
-                                            +"You have lost the game :(\n");
-                        }
-                        if(computerSelection==0){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[1]+"\n"
-                                            +"You have win the game :D\n");
-                        }
-                    case "SCISSORS":
-                        if(computerSelection==0){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[2]+"\n"
-                                            +"You have lost the game :(\n");
-                        }
-                        if(computerSelection==1){
-                            return console.log("Machine: "+alternatives[computerSelection]+"\n"
-                                            +"Player: "+alternatives[2]+"\n"
-                                            +"You have win the game :D\n");
-                        }
-                }
+    if(choice!=="ROCK"&&choice!=="PAPER"&&choice!=="SCISSORS"){
+        return alert("Not a valid choice! Thats an end game :(");
+    }
+    switch(choice){
+        case "ROCK":
+            if(computerSelection==1){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[0]+"\n"
+                                +"You have lost the game :(\n");
             }
+            if(computerSelection==2){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[0]+"\n"
+                                +"You have win the game :D\n");
+            }
+        case "PAPER":
+            if(computerSelection==2){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[1]+"\n"
+                                +"You have lost the game :(\n");
+            }
+            if(computerSelection==0){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[1]+"\n"
+                                +"You have win the game :D\n");
+            }
+        case "SCISSORS":
+            if(computerSelection==0){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[2]+"\n"
+                                +"You have lost the game :(\n");
+            }
+            if(computerSelection==1){
+                return console.log("Machine: "+alternatives[computerSelection]+"\n"
+                                +"Player: "+alternatives[2]+"\n"
+                                +"You have win the game :D\n");
+            }
+    }
+}
 
-            playRound(playerChoice);
+playRound(playerChoice);
